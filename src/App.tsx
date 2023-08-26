b880b9f4a9e9c021e88f4ebd95a38c06abadfd7c
Pavel Novik <brn2befree@gmail.com>
2023-08-26 18:56:06 +0300

    created Work section, add Work card elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import './App.css';
 // import styled from 'styled-components';
-import { Header } from "./layout/header/Header";
-import { Main } from './layout/sections/main/Main';
+import {Header} from "./layout/header/Header";
+import {Main} from './layout/sections/main/Main';
 import {Skills} from "./layout/sections/skills/Skills";
+import {Works} from "./layout/sections/works/Works";
 
 function App() {
     return (
         <div className="App">
-            <Header />
-            <Main />
-            <Skills />
-
+            <Header/>
+            <Main/>
+            <Skills/>
+            <Works/>
         </div>
     );
 }
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,33 +1,35 @@
 import React from 'react';
 import styled from "styled-components";
 
-export const Menu = () => {
+// type MenuPropsType = {
+//     menuItems: Array<string>
+// }
+export const Menu = (props: { menuItems: Array<string> }) => {
     return (
         <StyledMenu>
             <ul>
-                <li><a href="#">home</a></li>
-                <li><a href="#">slills</a></li>
-                <li><a href="#">works</a></li>
-                <li><a href="#">testimony</a></li>
-                <li><a href="#">contacts</a></li>
+                {props.menuItems.map((item, index) => {
+                    return <li key={index}><a href="#">{item}</a></li>
+                })}
             </ul>
         </StyledMenu>
     );
 };
 
 const StyledMenu = styled.nav`
-    ul {
-      display: flex;
-      list-style: none;
-      gap: 30px;
-      
-      li {
-        
-        a {
-          text-decoration: none;
-          text-transform: capitalize;
-        }
+  ul {
+    display: flex;
+    list-style: none;
+    gap: 30px;
+    justify-content: center;
+
+    li {
+
+      a {
+        text-decoration: none;
+        text-transform: capitalize;
       }
     }
+  }
 
 `
