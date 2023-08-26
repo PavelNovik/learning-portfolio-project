cdcc20635b609ea36cca2b75df61e3225b4bb9f5
Pavel Novik <brn2befree@gmail.com>
2023-08-26 16:25:30 +0300

    created main section, add flex wrapper

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styled from "styled-components";
-import {Logo} from "../../components/logo/Logo";
-import {Menu} from "../../components/menu/Menu";
+import { Logo } from "../../components/logo/Logo";
+import { Menu } from "../../components/menu/Menu";
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu/>
+            <Logo />
+            <Menu />
 
         </StyledHeader>
     );
---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+// import photoUrlWeb from '../../../assets/images/photo.webp';
+import photoUrlJpg from '../../../assets/images/photo.jpg';
+import styled from 'styled-components';
+import {FlexWrapper} from "../../../components/FlexWrapper";
+
+
+export const Main = () => {
+    return (
+        <StyledMain>
+            <FlexWrapper align={"center"} justify={"space-around"}>
+                <div>
+                <span>Hi There</span>
+                    <Name>I am Svetlana Dyablo</Name>
+                    <MainTitle>A Web Developer.</MainTitle>
+
+                </div>
+            <Photo src={photoUrlJpg} alt='my photo' />
+            </FlexWrapper>
+        </StyledMain>
+    )
+}
+
+const StyledMain = styled.div`
+min-height: 100vh;
+  background-color: beige;
+`
+
+const Photo = styled.img`
+width: 350px;
+height: 430px;
+object-fit: cover
+`
+const MainTitle = styled.h1``
+
+const Name = styled.h2``
