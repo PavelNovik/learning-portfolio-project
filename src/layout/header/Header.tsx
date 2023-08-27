d32b99a1ac6762b14e18f278e9bdd17bda6208b5
Pavel Novik <brn2befree@gmail.com>
2023-08-27 21:02:21 +0300

    add hover effect for menu, change header menu file

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import styled from "styled-components";
-import { Logo } from "../../components/logo/Logo";
-import { Menu } from "../../components/menu/Menu";
+import {Logo} from "../../components/logo/Logo";
+import {Container} from "../../components/Container";
+import {FlexWrapper} from "../../components/FlexWrapper";
+import {Theme} from "../../styles/Theme";
+import {HeaderMenu} from "./headerMenu/HeaderMenu";
 
 const items = ["home", "skills", "works", "testimony", "contacts"]
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo />
-            <Menu menuItems={items}/>
+            <Container>
+                <FlexWrapper justify={"space-between"} align={"center"}>
+                    <Logo/>
+                    <HeaderMenu menuItems={items}/>
+
+                </FlexWrapper>
+
+            </Container>
 
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
-  background-color: aquamarine;
-  display: flex;
-  justify-content: space-between;
+  background-color: ${Theme.colors.primaryBg};
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+
 `
