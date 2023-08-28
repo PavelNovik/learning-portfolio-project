ba437cfdf5e94589cc515c619c74f25d7e6e433f
Pavel Novik <brn2befree@gmail.com>
2023-08-28 22:09:14 +0300

    refactored Footer section

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from "styled-components";
 import {Icon} from "../../components/icon/Icon";
 import {FlexWrapper} from "../../components/FlexWrapper";
+import {Theme} from "../../styles/Theme";
 
 const socialList = ["instagram", "telegram", "vk", "linkedIn"]
 
@@ -28,23 +29,47 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-  min-height: 20vh;
-  background-color: #ffe97d;
+  background-color: ${Theme.colors.primaryBg};
+  padding: 40px 0;
 `
 
 const Name = styled.span`
-
+  font-family: "Josefin Sans", sans-serif;
+  font-size: 22px;
+  font-weight: 700;
+  letter-spacing: 3px;
 `
 
 const SocialIconsList = styled.ul`
   list-style: none;
   display: flex;
-  gap: 30px;
-  
+  gap: 20px;
+  margin: 30px 0;
 `
 
 const SocialIconItem = styled.li``
 
-const SocialIconLink = styled.a``
+const SocialIconLink = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  background: rgba(255, 255, 255, 0.10);
+  
+  color: ${Theme.colors.accent};
+  
+  &:hover {
+    color: ${Theme.colors.primaryBg};
+    background-color: ${Theme.colors.accent};
+    transform: translateY(-4px);
+  }
+`
 
-const Copyright = styled.small``
+const Copyright = styled.small`
+  text-align: center;
+  opacity: 0.5;
+  font-size: 12px;
+  font-weight: 400;
+`
