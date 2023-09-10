6b10fc76fc480e5eab9a6e8e38c567aa9f150d4d
Pavel Novik <brn2befree@gmail.com>
2023-09-10 12:20:46 +0300

    add close burger menu

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -3,18 +3,18 @@ import {S} from "../HeaderMenu_Styles";
 import {Menu} from "../menu/Menu";
 
 export const MobileMenu: React.FC<{ menuItems: Array<string> }> = (props: { menuItems: Array<string> }) => {
-    const [menuStatus, setMenuStatus] = React.useState(false);
-    const handleOpenMenu = () => menuStatus ? setMenuStatus(false) : setMenuStatus(true);
+    const [menuISOpen, setMenuIsOpen] = React.useState(false);
+    const handleOpenMenu = () => setMenuIsOpen(!menuISOpen);
 
 
 
     return (
         <S.MobileMenu>
-            <S.BurgerButton className={"burger-button"} onClick={handleOpenMenu} isOpen={menuStatus}>
+            <S.BurgerButton className={"burger-button"} onClick={handleOpenMenu} isOpen={menuISOpen}>
                 <span></span>
             </S.BurgerButton>
 
-            <S.MobileMenuPopup isOpen={menuStatus}>
+            <S.MobileMenuPopup isOpen={menuISOpen} onClick={() => {setMenuIsOpen(false)}}>
                 <Menu menuItems={props.menuItems}/>
             </S.MobileMenuPopup>
         </S.MobileMenu>
